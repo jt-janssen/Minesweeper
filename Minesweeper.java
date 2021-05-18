5a4a36f4b4d7e5782b279e1f24c8c48c86e3e3d0
JT Janssen <jt_janssen@icloud.com>
2021-05-17 21:16:44 -0700

    Added javadoc comments

--- Minesweeper.java
+++ Minesweeper.java
@@ -1,8 +1,16 @@
-public class Minesweeper {
+/**
+ * Command line Minesweeper game
+ * @author JT Janssen
+ */
 
+
+public class Minesweeper {
     Board board;
     int numOfBombs;
 
+    // Variables for colored terminal printouts
+    // Can only be used with supported terminals - Visual Studio Code, iTerm2, PowerShell, etc.
+    // Can be removed if buggy
     static final String ANSI_RESET = "\u001B[0m";
     static final String ANSI_BLACK = "\u001B[30m";
     static final String ANSI_RED = "\u001B[31m";
@@ -13,6 +21,7 @@ public class Minesweeper {
     static final String ANSI_CYAN = "\u001B[36m";
     static final String ANSI_WHITE = "\u001B[37m";
 
+    //Variables for tile values
     static final String TILE_BOMB = "*";
     static final String TILE_1 = ANSI_BLUE + "1" + ANSI_RESET;
     static final String TILE_2 = ANSI_GREEN + "2" + ANSI_RESET;
@@ -22,15 +31,20 @@ public class Minesweeper {
     static final String TILE_6 = ANSI_CYAN + "6" + ANSI_RESET;
     static final String TILE_7 = ANSI_BLACK + "7" + ANSI_RESET;
     static final String TILE_8 = "8";
-    static final String TILE_EMPTY = " ";
+    static final String TILE_0 = " ";
 
 
-
+    /**
+     * Runs Minesweeper
+     * @param args Not used
+     */
     public static void main(String[] args) {
         Minesweeper minesweeper = new Minesweeper(10, 10);
         clearScreen();
         printTitle();
         minesweeper.board.printBoard();
+
+        //runs uncoverTile() until it outputs false //TODO change this to a state machine & case for winning
         while(minesweeper.uncoverTile()){
             clearScreen();
             printTitle();
@@ -39,29 +53,28 @@ public class Minesweeper {
         System.out.println(ANSI_RED + "Game over." + ANSI_RESET);
     }
 
+    /**
+     * Constructs a minesweeper board from Board class
+     * @param dimension Side length of board (always square)
+     * @param numOfBombs Number of bombs on board
+     */
     Minesweeper(int dimension, int numOfBombs){
-        // board = new Board(dimension, 'X', false);
+        //creates a board object
         board = new Board(dimension, " ", "X", false);
         this.numOfBombs = numOfBombs;
-        placeBombs();
         fillBoard();
     }
 
-    static void printTitle(){
-        System.out.println("\n __  __ _\n|  \\/  (_)" +
-        "\n| \\  / |_ _ __   ___  _____      _____  ___ _ __   ___ _ __"+
-        "\n| |\\/| | | '_ \\ / _ \\/ __\\ \\ /\\ / / _ \\/ _ \\ '_ \\ / _ \\ '__|"+
-        "\n| |  | | | | | |  __/\\__ \\  V  V /  __/  __/ |_) |  __/ |"+
-        "\n|_|  |_|_|_| |_|\\___||___/ \\_/\\_/ \\___|\\___| .__/ \\___|_|"+
-        "\n                                           | |"+
-        "\n                                           |_|");
-    }
-
+    /**
+     * Gets user input and uncovers tiles 
+     * @return false if bomb is uncovered, true otherwise 
+     */
     boolean uncoverTile(){
         String input = board.getInput("Enter a tile to uncover: ", "Not a valid tile, enter another.");
         board.changeVisibility(input, true);
-        if(board.getTile(input) == TILE_EMPTY){
-            board.uncoverTilesAroundAll(TILE_EMPTY);
+
+        if(board.getTile(input) == TILE_0){
+            board.uncoverTilesAroundAll(TILE_0);
             return true;
         } else if (board.getTile(input) == TILE_BOMB){
             return false;
@@ -70,7 +83,11 @@ public class Minesweeper {
         }
     }
 
-    private void placeBombs(){
+    /**
+     * Fills the board with randomly placed bombs and number tiles
+     */
+    void fillBoard(){
+        //places bombs randomly
         for(int i = 0; i < numOfBombs; i++){
             String randomTile = board.getRandomTilePos();
             if(board.getTile(randomTile) != TILE_BOMB){
@@ -79,14 +96,15 @@ public class Minesweeper {
                 i--;
             }
         }
-    } 
 
-    private void fillBoard(){
+        //places number tiles
         int counter;
+        //loops through each tile on the board
         for(int row = 0; row < board.dimension; row++){
             for(int col = 0; col < board.dimension; col++){
                 String currentPos = board.getPosSouth(board.getPosEast("A1", col), row);
                 counter = 0;
+                //counts how many bombs are around
                 if(board.getTile(currentPos) != TILE_BOMB){
                     if(board.getTile(board.getPosNorth(currentPos, 1)) == TILE_BOMB){
                         counter++;
@@ -113,23 +131,24 @@ public class Minesweeper {
                         counter++;
                     }
                     
+                    //sets tile based on how many bombs are around
                     if(counter == 0){
-                        board.setTile(currentPos, TILE_EMPTY);
+                        board.setTile(currentPos, TILE_0);
                     } else if (counter  == 1){
                         board.setTile(currentPos, TILE_1);
-                    }else if (counter  == 2){
+                    } else if (counter  == 2){
                         board.setTile(currentPos, TILE_2);
-                    }else if (counter  == 3){
+                    } else if (counter  == 3){
                         board.setTile(currentPos, TILE_3);
-                    }else if (counter  == 4){
+                    } else if (counter  == 4){
                         board.setTile(currentPos, TILE_4);
-                    }else if (counter  == 5){
+                    } else if (counter  == 5){
                         board.setTile(currentPos, TILE_5);
-                    }else if (counter  == 6){
+                    } else if (counter  == 6){
                         board.setTile(currentPos, TILE_6);
-                    }else if (counter  == 7){
+                    } else if (counter  == 7){
                         board.setTile(currentPos, TILE_7);
-                    }else if (counter  == 8){
+                    } else if (counter  == 8){
                         board.setTile(currentPos, TILE_8);
                     }
                 }
@@ -137,7 +156,21 @@ public class Minesweeper {
         }
     }
 
-    public static void clearScreen() {  
+    /**
+     * Prints Minesweeper title
+     */
+    static void printTitle(){
+        System.out.println("\n __  __ _\n|  \\/  (_)" +
+        "\n| \\  / |_ _ __   ___  _____      _____  ___ _ __   ___ _ __"+
+        "\n| |\\/| | | '_ \\ / _ \\/ __\\ \\ /\\ / / _ \\/ _ \\ '_ \\ / _ \\ '__|"+
+        "\n| |  | | | | | |  __/\\__ \\  V  V /  __/  __/ |_) |  __/ |"+
+        "\n|_|  |_|_|_| |_|\\___||___/ \\_/\\_/ \\___|\\___| .__/ \\___|_|"+
+        "\n                                           | |"+
+        "\n                                           |_|");
+    }
+
+    // used to clear the terminal screen
+    static void clearScreen() {  
         System.out.print("\033[H\033[2J");  
         System.out.flush();  
     }
