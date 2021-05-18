fd685a90af79407aa519a716bda998d54c2685f0
JT Janssen <jt_janssen@icloud.com>
2021-05-17 20:44:09 -0700

    Incorporated new Board class

--- Board.java
+++ Board.java
@@ -1,70 +1,141 @@
 import java.util.*;
 
-public class Board {
-    private char[][] board;
-    private Hashtable<Character, String> dictionary = new Hashtable<Character, String>();
-    private Hashtable<String, Boolean> visibleSet = new Hashtable<String, Boolean>();
+/**
+ * Command line game board structure
+ * @author JT Janssen
+*/
+/* Uses an alphanumeric coordinate system where:
+ *  - Letters are columns on the horizontal axis, and
+ *  - Numbers are rows on the vertical axis
+ *  - Eg. A4, F1, G10, etc.
+ * 
+ * Each tile has two main values:
+ *  - The String that is stored in the matrix
+ *  - The visibility boolean
+ * 
+ * If a tile is visible, the matrix value will be printed
+ * If a tile is hidden, the hiddenValue will be displayed
+ * 
+ * This is beneficial for games where data needs to be stored on the board,
+ * but cannot be shown to the player yet
+ */
 
+public class Board {
+    private String[][] board;
+    private Hashtable<String, Boolean> visibleSet = new Hashtable<String, Boolean>();
 
     private Random random = new Random();
     private Scanner keys = new Scanner(System.in);
 
     int dimension;
-    char defaultValue;
+    String initialValue, hiddenValue;
 
-    String[] tiles;
-
-    // <------Example main method code------->
-
-    
+    /**
+     * Example main method code
+     * @param args Not used
+     */
+    /*
+     * Creates a Board object, board, with 10x10 dimensions, "x" as the default value, " " as the hidden value, and al tiles set to hidden
+     * Prints board - all tiles are " "
+     * Changes the visibility of tile A3 to visible
+     * Prints board - tile A3 is "x"
+     */
     public static void main(String[] args) {
-        Board board = new Board(10, ' ', false);
-        board.setDictionary(' ', ".");
+        Board board = new Board(10, "x", " ", false);
         board.printBoard(); 
-        for(int i = 0; i < board.dimension * board.dimension; i++){
-            System.out.println(board.iterateTiles(i));
-        }       
+        board.changeVisibility("A3", true);   
+        board.printBoard(); 
+    
     }
 
-    
-
-    Board(int dimension, char defaultValue, boolean defaultVisibility){
+    /**
+     * Creates the Board object
+    * @param dimension Side length of board (always square)
+    * @param initialValue Value to fill board with initally, shown when visible - can include color escape sequences, but the printable characters should be 1 character long (chars, emojis, etc.)
+    * @param hiddenValue Value to show when tile is not visible - same as above, printable characters should be 1 charater long
+    * @param initialVisibility State of visibility at beginning - all tiles
+    */
+    public Board(int dimension, String initialValue, String hiddenValue, boolean initialVisibility){
         if(dimension < 1){
             dimension = 1;
         }
         this.dimension = dimension;
-        this.defaultValue = defaultValue;
-        board = new char[dimension + 2][dimension + 2];
+        this.initialValue = initialValue;
+        this.hiddenValue = hiddenValue;
+        board = new String[dimension + 2][dimension + 2];
         for(int i = 0; i < dimension + 2; i++){
             for(int j = 0; j < dimension + 2; j++){
-                board[i][j] = defaultValue;
+                board[i][j] = initialValue;
             }
         }
-        setVisibleSet(defaultVisibility);
+        setVisibleSet(initialVisibility);
     }
 
 
-    boolean isPosVisible(String pos){
+    /**
+     * Returns true if the input position is visible
+     * @param pos Alphanumeric coordinate
+     * @return True if visible, false if hidden
+     */
+    public boolean isPosVisible(String pos){
         return visibleSet.get(pos);
     }
 
-    void changeVisibility(String pos, boolean visibility){
+    /**
+     * Changes the visibility of the input position
+     * @param pos Alphanumeric coordinate
+     * @param visibility True to set visible, false for hidden
+     */
+    public void changeVisibility(String pos, boolean visibility){
         visibleSet.put(pos, visibility);
+        // System.out.print("visi changed");
+
     }
 
-    private void setVisibleSet(boolean defaultValue){
+    //Used to set each tile to the default value when creating object
+    private void setVisibleSet(boolean visibility){
         for(int row = -1; row < dimension + 1; row++){  //these values are so that it will include the border tiles
             for(int col = -1; col < dimension + 1; col++){
-                visibleSet.put(getPosSouth(getPosEast("A1", col), row), defaultValue);
+                visibleSet.put(getPosSouth(getPosEast("A1", col), row), visibility);
             }
         }
     }
 
-    String iterateTiles(int x){
-        return "" + ((char) (65 + x % dimension)) + ((x / dimension) + 1);
+    /**
+     * Clears all tiles around a certain visible value
+     * @param value Tile value to continually uncover
+     */
+    /*
+     * Used to clear all tiles of a certain value (eg. Minesweeper when a blank square is uncovered)
+     * Loops through the matrix, and if there is a tile of the specified value that is visible, it makes all surrounding tiles visible.
+     * Continues until no tiles are made visible.
+     */
+    public void uncoverTilesAroundAll(String value){
+        boolean tileChanged = true;
+        String currentPos;
+        while(tileChanged){
+            tileChanged = false;
+            for(int i = 0; i < dimension * dimension; i++){
+                currentPos = "" + ((char) (65 + i % dimension)) + ((i / dimension) + 1);
+                if(isPosVisible(currentPos) && getTile(currentPos) == value && (!isPosVisible(getPosNorth(currentPos, 1)) || !isPosVisible(getPosSouth(currentPos, 1)) || !isPosVisible(getPosEast(currentPos, 1)) || !isPosVisible(getPosWest(currentPos, 1)) || !isPosVisible(getPosNW(currentPos)) || !isPosVisible(getPosNE(currentPos)) || !isPosVisible(getPosSW(currentPos)) || !isPosVisible(getPosSE(currentPos)))){
+                    changeVisibility(getPosNorth(currentPos, 1), true);
+                    changeVisibility(getPosSE(currentPos), true);
+                    changeVisibility(getPosSW(currentPos), true);
+                    changeVisibility(getPosNE(currentPos), true);
+                    changeVisibility(getPosNW(currentPos), true);
+                    changeVisibility(getPosWest(currentPos, 1), true);
+                    changeVisibility(getPosEast(currentPos, 1), true);
+                    changeVisibility(getPosSouth(currentPos, 1), true);
+                    tileChanged = true;
+                }
+            }
+        }
     }
 
-    void printBoard(){
+    /**
+     * Prints the board to the console with coordinates
+     */
+    public void printBoard(){
         //Prints out letters above grid
         System.out.print("     ");
         for(int i = 0; i < dimension; i++){
@@ -89,12 +160,13 @@ public class Board {
 
             //prints out elements
             for(int boardColumn = 1; boardColumn <= dimension; boardColumn++){
-                for (char key : dictionary.keySet()) {
-                    if(board[boardRow][boardColumn] == key && visibleSet.get(getPos(boardRow, boardColumn))){
-                        System.out.print(dictionary.get(key) + " | ");
-                    } else if (board[boardRow][boardColumn] == key && !visibleSet.get(getPos(boardRow, boardColumn))){
-                        System.out.print(defaultValue + " | ");
-                    }
+                if(visibleSet.get(getPos(boardRow, boardColumn))){  //if the tile is visible
+                    System.out.print(board[boardRow][boardColumn] + " | ");
+                    // System.out.print("true");
+
+                } else if (!visibleSet.get(getPos(boardRow, boardColumn))){ //if the tile is not visible
+                    System.out.print(hiddenValue + " | ");
+                    // System.out.print("false");
                 }
             }
             System.out.print("\n");
@@ -107,7 +179,8 @@ public class Board {
         System.out.print("\n");
     }
 
-    private boolean isPosValid(String pos){
+    // Returns true if the input position is within the visible matrix and not in border
+    private boolean isPosValid(String pos){ //to avoid placing tiles in border
         if(getRowIndex(pos) >= 1 && getRowIndex(pos) <= dimension && getColIndex(pos) >= 1 && getColIndex(pos) <= dimension){
             return true;
         } else {
@@ -115,7 +188,13 @@ public class Board {
         }
     }
 
-    String getInput(String inputMessage, String errorMessage){
+    /**
+     * Gets a valid position from the user
+     * @param inputMessage The printed message to recieve input (add \n to end if a line break is wanted)
+     * @param errorMessage The error message printed if input is not valid
+     * @return Returns the valid position (eg. E3)
+     */
+    public String getInput(String inputMessage, String errorMessage){
         System.out.print(inputMessage);
         String input = keys.next();
         if(isPosValid(input)){  
@@ -123,60 +202,106 @@ public class Board {
         } else {
             System.out.println(errorMessage);
             input = "";
-            return getInput(inputMessage, errorMessage);   //recursion - repeats the method until condition is satisfied
+            return getInput(inputMessage, errorMessage);
         }
     }
-    char getTile(String pos){
+
+    /**
+     * Gets the value of the value in the tile (whether hidden or not)
+     * @param pos Alphanumeric coordinate
+     * @return String value of tile
+     */
+    public String getTile(String pos){
         return board[getRowIndex(pos)][getColIndex(pos)];
     }
-    void setTile(String pos, char value){
+
+    /**
+     * Sets the tile value of a specified coordinate
+     * @param pos Alphanumeric coordinate
+     * @param value Value to assign tile - can include color escape sequences, but the printable characters should be 1 character long (chars, emojis, etc.)
+     */
+    public void setTile(String pos, String value){
         board[getRowIndex(pos)][getColIndex(pos)] = value;
     }
-    private int getRowIndex(String pos){
+
+    /**
+     * Used to convert the alplanumeric coordinate into row index value
+     * @param pos Alphanumeric coordinate
+     * @return Index value of row
+     */
+    // Calculated by creating a substring from the input string staring at index 1, then converting that to an integer
+    public int getRowIndex(String pos){
         //offset by 1 to account for extra rows
         return Integer.parseInt(pos.substring(1));
     }
 
-    private int getColIndex(String pos){
+    /**
+     * Used to convert the alplanumeric coordinate into column index value
+     * @param pos Alphanumeric coordinate
+     * @return Index value of column
+     */
+    // Calculated by taking the first character's ASCII value, and shifting it so that A = 1
+    public int getColIndex(String pos){
         //offset by 1 to account for extra columns
         return (int) pos.charAt(0) - 64;
     }
 
-    void setDictionary(char hidden, String visible){
-        dictionary.put(hidden, visible);
-    }
-
-    String getPos(int row, int column){
+    /**
+     * Returns the alphanumeric coordinate based on row and column index
+     * @param row Index value of row
+     * @param column Index value of column
+     * @return Alphanumeric coordinate
+     */
+    public String getPos(int row, int column){
         return "" + ((char) (64 + column)) + (row);
     }
 
-    String getRandomTilePos(){
+    /**
+     * Gets a random tile on the board
+     * @return Random alphanumeric coordinate
+     */
+    public String getRandomTilePos(){
         return "" + ((char) (random.nextInt((65 + dimension)  - 65) + 65)) + (random.nextInt((dimension + 1) - 1) + 1);
     }
 
-    String getPosSouth(String pos, int n){
-        return String.valueOf((char) pos.charAt(0)) + (Integer.parseInt(pos.substring(1)) + n);
-    }
-    String getPosNorth(String pos, int n){
+
+    // The following methods return the alphanumeric coordinate of the position in the specified direction
+    // If an int is specified, that is how many tiles adjacent it looks
+
+    /**
+     * Returns the nth position north of the coordinate
+     * @param pos Alphanumeric coordinate
+     * @param n n number of tiles north
+     * @return Alphanumeric coordinate n tiles north of pos
+     */
+    public String getPosNorth(String pos, int n){
         return String.valueOf((char) pos.charAt(0)) + (Integer.parseInt(pos.substring(1)) - n);
     }
-    String getPosWest(String pos, int n){
+    public String getPosSouth(String pos, int n){
+        return String.valueOf((char) pos.charAt(0)) + (Integer.parseInt(pos.substring(1)) + n);
+    }
+    public String getPosWest(String pos, int n){
         return String.valueOf((char) (pos.charAt(0) - n)) + (Integer.parseInt(pos.substring(1)));
     }
-    String getPosEast(String pos, int n){
+    public String getPosEast(String pos, int n){
         return String.valueOf((char) (pos.charAt(0) + n)) + (Integer.parseInt(pos.substring(1)));
     }
 
-    String getPosSW(String pos){
+    /**
+     * Returns the position Southwest of the coordinate
+     * @param pos Alphanumeric coordinate
+     * @return Alphanumeric coordinate Southwest of pos
+     */
+    public String getPosSW(String pos){
         return String.valueOf((char) (pos.charAt(0) - 1)) + (Integer.parseInt(pos.substring(1)) + 1);
     }
-    String getPosSE(String pos){
+    public String getPosSE(String pos){
         return String.valueOf((char) (pos.charAt(0) + 1)) + (Integer.parseInt(pos.substring(1)) + 1);
     }
-    String getPosNE(String pos){
+    public String getPosNE(String pos){
         return String.valueOf((char) (pos.charAt(0) + 1)) + (Integer.parseInt(pos.substring(1)) - 1);
     }
-    String getPosNW(String pos){
+    public String getPosNW(String pos){
         return String.valueOf((char) (pos.charAt(0) - 1)) + (Integer.parseInt(pos.substring(1)) - 1);
     }
 }
--- Minesweeper.java
+++ Minesweeper.java
@@ -1,4 +1,5 @@
 public class Minesweeper {
+
     Board board;
     int numOfBombs;
 
@@ -12,30 +13,57 @@ public class Minesweeper {
     static final String ANSI_CYAN = "\u001B[36m";
     static final String ANSI_WHITE = "\u001B[37m";
 
+    static final String TILE_BOMB = "*";
+    static final String TILE_1 = ANSI_BLUE + "1" + ANSI_RESET;
+    static final String TILE_2 = ANSI_GREEN + "2" + ANSI_RESET;
+    static final String TILE_3 = ANSI_RED + "3" + ANSI_RESET;
+    static final String TILE_4 = ANSI_PURPLE + "4" + ANSI_RESET;
+    static final String TILE_5 = ANSI_RED + "5" + ANSI_RESET;
+    static final String TILE_6 = ANSI_CYAN + "6" + ANSI_RESET;
+    static final String TILE_7 = ANSI_BLACK + "7" + ANSI_RESET;
+    static final String TILE_8 = "8";
+    static final String TILE_EMPTY = " ";
+
+
+
     public static void main(String[] args) {
         Minesweeper minesweeper = new Minesweeper(10, 10);
+        clearScreen();
+        printTitle();
         minesweeper.board.printBoard();
         while(minesweeper.uncoverTile()){
+            clearScreen();
+            printTitle();
             minesweeper.board.printBoard();
         }
         System.out.println(ANSI_RED + "Game over." + ANSI_RESET);
     }
 
     Minesweeper(int dimension, int numOfBombs){
-        board = new Board(dimension, 'X', false);
+        // board = new Board(dimension, 'X', false);
+        board = new Board(dimension, " ", "X", false);
         this.numOfBombs = numOfBombs;
-        defineChars();
         placeBombs();
         fillBoard();
+    }
+
+    static void printTitle(){
+        System.out.println("\n __  __ _\n|  \\/  (_)" +
+        "\n| \\  / |_ _ __   ___  _____      _____  ___ _ __   ___ _ __"+
+        "\n| |\\/| | | '_ \\ / _ \\/ __\\ \\ /\\ / / _ \\/ _ \\ '_ \\ / _ \\ '__|"+
+        "\n| |  | | | | | |  __/\\__ \\  V  V /  __/  __/ |_) |  __/ |"+
+        "\n|_|  |_|_|_| |_|\\___||___/ \\_/\\_/ \\___|\\___| .__/ \\___|_|"+
+        "\n                                           | |"+
+        "\n                                           |_|");
     }
 
     boolean uncoverTile(){
         String input = board.getInput("Enter a tile to uncover: ", "Not a valid tile, enter another.");
         board.changeVisibility(input, true);
-        if(board.getTile(input) == '0'){
-            tileSweep();
+        if(board.getTile(input) == TILE_EMPTY){
+            board.uncoverTilesAroundAll(TILE_EMPTY);
             return true;
-        } else if (board.getTile(input) == '*'){
+        } else if (board.getTile(input) == TILE_BOMB){
             return false;
         } else {
             return true;
@@ -45,48 +73,13 @@ public class Minesweeper {
     private void placeBombs(){
         for(int i = 0; i < numOfBombs; i++){
             String randomTile = board.getRandomTilePos();
-            if(board.getTile(randomTile) != '*'){
-                board.setTile(randomTile, '*');
+            if(board.getTile(randomTile) != TILE_BOMB){
+                board.setTile(randomTile, TILE_BOMB);
             } else {
                 i--;
             }
         }
-    }
-
-    private void defineChars(){
-        board.setDictionary('*', "*");
-        board.setDictionary('1', ANSI_BLUE + "1" + ANSI_RESET);
-        board.setDictionary('2', ANSI_GREEN + "2" + ANSI_RESET);
-        board.setDictionary('3', ANSI_RED + "3" + ANSI_RESET);
-        board.setDictionary('4', ANSI_PURPLE + "4" + ANSI_RESET);
-        board.setDictionary('5', ANSI_RED + "5" + ANSI_RESET);
-        board.setDictionary('6', ANSI_RED + "6" + ANSI_RESET);
-        board.setDictionary('7', ANSI_RED + "7" + ANSI_RESET);
-        board.setDictionary('8', ANSI_RED + "8" + ANSI_RESET);
-        board.setDictionary('0', " ");
-    }
-
-    private void tileSweep(){
-        boolean tileChanged = true;
-        String currentPos;
-        while(tileChanged){
-            tileChanged = false;
-            for(int i = 0; i < board.dimension * board.dimension; i++){
-                currentPos = board.iterateTiles(i);
-                if(board.isPosVisible(currentPos) && board.getTile(currentPos) == '0' && (!board.isPosVisible(board.getPosNorth(currentPos, 1)) || !board.isPosVisible(board.getPosSouth(currentPos, 1)) || !board.isPosVisible(board.getPosEast(currentPos, 1)) || !board.isPosVisible(board.getPosWest(currentPos, 1)) || !board.isPosVisible(board.getPosNW(currentPos)) || !board.isPosVisible(board.getPosNE(currentPos)) || !board.isPosVisible(board.getPosSW(currentPos)) || !board.isPosVisible(board.getPosSE(currentPos)))){
-                    board.changeVisibility(board.getPosNorth(currentPos, 1), true);
-                    board.changeVisibility(board.getPosSE(currentPos), true);
-                    board.changeVisibility(board.getPosSW(currentPos), true);
-                    board.changeVisibility(board.getPosNE(currentPos), true);
-                    board.changeVisibility(board.getPosNW(currentPos), true);
-                    board.changeVisibility(board.getPosWest(currentPos, 1), true);
-                    board.changeVisibility(board.getPosEast(currentPos, 1), true);
-                    board.changeVisibility(board.getPosSouth(currentPos, 1), true);
-                    tileChanged = true;
-                }
-            }
-        }
-    }
+    } 
 
     private void fillBoard(){
         int counter;
@@ -94,34 +87,58 @@ public class Minesweeper {
             for(int col = 0; col < board.dimension; col++){
                 String currentPos = board.getPosSouth(board.getPosEast("A1", col), row);
                 counter = 0;
-                if(board.getTile(currentPos) != '*'){
-                    if(board.getTile(board.getPosNorth(currentPos, 1)) == '*'){
+                if(board.getTile(currentPos) != TILE_BOMB){
+                    if(board.getTile(board.getPosNorth(currentPos, 1)) == TILE_BOMB){
                         counter++;
                     }
-                    if(board.getTile(board.getPosSouth(currentPos, 1)) == '*'){
+                    if(board.getTile(board.getPosSouth(currentPos, 1)) == TILE_BOMB){
                         counter++;
                     }
-                    if(board.getTile(board.getPosEast(currentPos, 1)) == '*'){
+                    if(board.getTile(board.getPosEast(currentPos, 1)) == TILE_BOMB){
                         counter++;
                     }
-                    if(board.getTile(board.getPosWest(currentPos, 1)) == '*'){
+                    if(board.getTile(board.getPosWest(currentPos, 1)) == TILE_BOMB){
                         counter++;
                     }
-                    if(board.getTile(board.getPosNW(currentPos)) == '*'){
+                    if(board.getTile(board.getPosNW(currentPos)) == TILE_BOMB){
                         counter++;
                     }
-                    if(board.getTile(board.getPosNE(currentPos)) == '*'){
+                    if(board.getTile(board.getPosNE(currentPos)) == TILE_BOMB){
                         counter++;
                     }
-                    if(board.getTile(board.getPosSE(currentPos)) == '*'){
+                    if(board.getTile(board.getPosSE(currentPos)) == TILE_BOMB){
                         counter++;
                     }
-                    if(board.getTile(board.getPosSW(currentPos)) == '*'){
+                    if(board.getTile(board.getPosSW(currentPos)) == TILE_BOMB){
                         counter++;
                     }
-                    board.setTile(currentPos, (char) (counter + '0'));
+                    
+                    if(counter == 0){
+                        board.setTile(currentPos, TILE_EMPTY);
+                    } else if (counter  == 1){
+                        board.setTile(currentPos, TILE_1);
+                    }else if (counter  == 2){
+                        board.setTile(currentPos, TILE_2);
+                    }else if (counter  == 3){
+                        board.setTile(currentPos, TILE_3);
+                    }else if (counter  == 4){
+                        board.setTile(currentPos, TILE_4);
+                    }else if (counter  == 5){
+                        board.setTile(currentPos, TILE_5);
+                    }else if (counter  == 6){
+                        board.setTile(currentPos, TILE_6);
+                    }else if (counter  == 7){
+                        board.setTile(currentPos, TILE_7);
+                    }else if (counter  == 8){
+                        board.setTile(currentPos, TILE_8);
+                    }
                 }
             }
         }
+    }
+
+    public static void clearScreen() {  
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
     }
 }
